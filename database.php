eed36afc0232e0a90e003bf436212db468259e05
jeyashreevaithiam <jeyashreevaithiam@users.noreply.github.com>
2020-07-19 20:17:36 +0200

    adding database

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_SERVER', 'localhost'); // DB_server,DB_USER,DB_password is a constant
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'commercialdb');
+
+
+
+
+/*$server = 'localhost';
+$user = 'root';
+$password = '';*/
